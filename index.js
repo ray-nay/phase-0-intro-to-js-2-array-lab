57c15849cec764e18b0ea694e10d184bc6049023
Renee Mumbi <1723renee@gmail.com>
2022-11-20 21:40:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+const cats= ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+};
+function destructivelyRemoveLastCat(name) {
+    cats.pop("Bob");
+};
+function destructivelyRemoveFirstCat(name) {
+    cats.shift("Bob");
+};
+function appendCat(name){
+   const mumbi = [...cats,"Broom"];
+    return mumbi;
+};
+function prependCat(name){
+    const pet = ["Arnold",...cats];
+     return pet;
+ };
+
+
+
+
+  
+ function removeLastCat(){
+    return cats.slice(0,-1);
+ };
+ function removeFirstCat(){
+    return cats.slice(1);
+  };
